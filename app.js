d656863912e68d87607327b1260469527470c000
ayelencoppav <ayelencoppav@gmail.com>
2023-10-05 21:56:21 -0300

    agregue /turing

--- app.js
+++ app.js
@@ -26,7 +26,6 @@ app.get('/hamilton', function(req, res){
     console.log(__dirname)
 })
 
-<<<<<<< HEAD
 app.get('/hopper', function(req, res){
     res.sendFile(path.join(__dirname,'/views/hamilton.html'))
     console.log(__dirname)
@@ -36,9 +35,11 @@ app.get('/lovelace', function(req, res){
     res.sendFile(path.join(__dirname,'/views/hamilton.html'))
     console.log(__dirname)
 })
-=======
+app.get('/turing', function(req, res){
+    res.sendFile(path.join(__dirname,'/views/hamilton.html'))
+    console.log(__dirname)
+})
 app.use(express.static(path.join(__dirname, 'public')));
->>>>>>> 29d496c895f8c1c7faab513dbfb15a08b14f2204
 
 app.listen(3030,()=>{
     console.log("Servidor en marcha!");
